d3bb78047b45975f53d2807b45ea3769fffb3dfe
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-08-17 14:56:22 -0600

    chore: generate python samples templates in owlbot.py

--- owlbot.py
+++ owlbot.py
@@ -69,6 +69,7 @@ s.replace(
 # Add templated files
 # ----------------------------------------------------------------------------
 templated_files = common.py_library(
+python.py_samples(skip_readmes=True)
     samples=False,  # set to True only if there are samples
     microgenerator=True,
     cov_level=98,
