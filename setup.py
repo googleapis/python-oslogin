eb5c5c45f67f36410a83a9d6a9cb10214ad654ab
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2022-02-15 09:02:00 -0700

    fix: restore proto-plus dependency

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ dependencies = [
     # Until this issue is closed
     # https://github.com/googleapis/google-cloud-python/issues/10566
     "google-api-core[grpc] >= 1.28.0, <3.0.0dev",
+    "proto-plus >= 1.4.0",
 ]
 extras = {"libcst": "libcst >= 0.2.5"}
 
